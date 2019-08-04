b2081cde832e74a0d55a6606bd5f6a1cef200874
Logikoz <37851168+LogikozRC@users.noreply.github.com>
2019-08-03 23:46:58 -0500

    refatorando codigo e comentando.

--- Shipped/Shipped.ino
+++ Shipped/Shipped.ino
@@ -6,63 +6,73 @@ const unsigned short int SdaPin = 10;
 const unsigned short int RtsPin = 9;
 
 //array containing allowed employees 
-String RegistredEmployeeID[] = { "cardsIDHere" };
+String RegistredEmployeeID[] = { "34611b" };
 //create a object of type MFRC522
 MFRC522 ReadRFID(SdaPin, RtsPin);
 
 void setup()
 {
-	Serial.begin(9600);
-	SPI.begin();
-	ReadRFID.PCD_Init();
+  // Initialize serial communications with the PC
+  Serial.begin(9600);
+  // Init SPI bus
+  SPI.begin();
+  // Init MFRC522 card
+  ReadRFID.PCD_Init();
 }
 
 void loop()
 {
-	ReadCard();
+  ReadCard();
 }
-
+//keep employee card id
 String EmployeeID;
-bool isChecked = false;
+//if the id exists or not, it will be saved here
+bool isChecked;
 
 void ReadCard()
 {
-	EmployeeID = "";
-
-	if (ReadRFID.PICC_IsNewCardPresent() && ReadRFID.PICC_ReadCardSerial())
-	{
-		for (byte i = 0; i < ReadRFID.uid.size; i++)
-		{
-			EmployeeID.concat(String(ReadRFID.uid.uidByte[i], HEX));
-		}
-
-		for (int i = 0; i < (sizeof(RegistredEmployeeID) / sizeof(String)); i++)
-		{
-			isChecked = EmployeeID.equalsIgnoreCase(RegistredEmployeeID[i]);
-		}
-
-		if (isChecked == true)
-		{
-			AccessAllowed();
-		}
-		else
-		{
-			AccessDenied();
-		}
-		delay(3000);
-	}
-	else
-	{
-		return;
-	}
+  //initializing EmployeeID with empty for avoid bugs.
+  EmployeeID = "";
+  //checking if there is a card on top of the reader
+  if (ReadRFID.PICC_IsNewCardPresent() && ReadRFID.PICC_ReadCardSerial())
+  {
+    //reading id from card and saving to variable.
+    for (byte i = 0; i < ReadRFID.uid.size; i++)
+    {
+      EmployeeID.concat(String(ReadRFID.uid.uidByte[i], HEX));
+    }
+    //checking if the id is from an employee.
+    for (int i = 0; i < (sizeof(RegistredEmployeeID) / sizeof(String)); i++)
+    {
+      isChecked = EmployeeID.equalsIgnoreCase(RegistredEmployeeID[i]);
+    }
+    //if true, granted acess
+    if (isChecked == true)
+    {
+      AccessAllowed();
+    }
+    //else, i'm sorry.
+    else
+    {
+      AccessDenied();
+    }
+    //awaiting
+    delay(3000);
+  }
+  else
+  {
+    return;
+  }
 }
 
 void AccessAllowed()
 {
-	Serial.println("Allowed: " + EmployeeID);
+  //send message of acess allowed 
+  Serial.println("Allowed: " + EmployeeID);
 }
 
 void AccessDenied()
 {
-	Serial.println("Denide: " + EmployeeID);
+  //send message of acess denied 
+  Serial.println("Denide: " + EmployeeID);
 }

eaba2123a9aa81de1dfb1173fecf0ed700dbb18d
Logikoz <37851168+LogikozRC@users.noreply.github.com>
2019-08-03 22:52:36 -0500

    lendo cartoes e verificando

--- Shipped/Shipped.ino
+++ Shipped/Shipped.ino
@@ -1,15 +1,68 @@
-/*
- Name:    Shipped.ino
- Created: 03/08/2019 19:38:15
- Author:  Logikoz
-*/
+#include <SPI.h>
+#include <MFRC522.h>
+
+//pins
+const unsigned short int SdaPin = 10;
+const unsigned short int RtsPin = 9;
+
+//array containing allowed employees 
+String RegistredEmployeeID[] = { "cardsIDHere" };
+//create a object of type MFRC522
+MFRC522 ReadRFID(SdaPin, RtsPin);
 
 void setup()
 {
-	pinMode(12, OUTPUT);
+	Serial.begin(9600);
+	SPI.begin();
+	ReadRFID.PCD_Init();
 }
 
 void loop()
 {
-	
+	ReadCard();
+}
+
+String EmployeeID;
+bool isChecked = false;
+
+void ReadCard()
+{
+	EmployeeID = "";
+
+	if (ReadRFID.PICC_IsNewCardPresent() && ReadRFID.PICC_ReadCardSerial())
+	{
+		for (byte i = 0; i < ReadRFID.uid.size; i++)
+		{
+			EmployeeID.concat(String(ReadRFID.uid.uidByte[i], HEX));
+		}
+
+		for (int i = 0; i < (sizeof(RegistredEmployeeID) / sizeof(String)); i++)
+		{
+			isChecked = EmployeeID.equalsIgnoreCase(RegistredEmployeeID[i]);
+		}
+
+		if (isChecked == true)
+		{
+			AccessAllowed();
+		}
+		else
+		{
+			AccessDenied();
+		}
+		delay(3000);
+	}
+	else
+	{
+		return;
+	}
+}
+
+void AccessAllowed()
+{
+	Serial.println("Allowed: " + EmployeeID);
+}
+
+void AccessDenied()
+{
+	Serial.println("Denide: " + EmployeeID);
 }
